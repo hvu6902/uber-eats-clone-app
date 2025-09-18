5d62489faa0c2eaef285e444daf5ca96d00cec92
Vincent Hoang <hoangvupantc@gmail.com>
2025-09-18 11:33:46 -0400

    landing page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+    return(
+        <div className="flex flex-col gap-12">
+            <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+                <h1 className="text-5x1 font-bold tracking-tight text-orange-600">
+                    Tuck into your favorite meals, anytime, anywhere!
+                </h1>
+                <span className="text-x1">Food is just a click away</span>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
