cf9cadabf975bff695d30f3af2f83474362fb746
Vincent Hoang <hoangvupantc@gmail.com>
2025-09-19 01:19:39 -0400

    auth

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <div className="bg-orange-500 py-6 mt-10">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <span className="tex-3xl text-white font-bold tracking-tight">
+                Mohammed No Salad
+            </span>
+            <span className="text-white font-bold tracking-tight flex gap-4">
+                <span>Privacy Policy</span>
+                <span>Terms of Service</span>
+                <span>Contact Us</span>
+            </span>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import landingImage from '../assets/landing.png'
+import appDownloadImage from '../assets/appDownload.png'
+
 const HomePage = () => {
     return(
         <div className="flex flex-col gap-12">
@@ -6,6 +9,18 @@ const HomePage = () => {
                     Tuck into your favorite meals, anytime, anywhere!
                 </h1>
                 <span className="text-x1">Food is just a click away</span>
+            </div>
+            <div className="grid md:grid-cols-2 gap-5">
+                <img src={landingImage}/>
+                <div className='flex flex-col items0center justify0center gap-4 text-center'>
+                    <span className='font-bold text-3xl tracking-tighter'>
+                        Bring the whoole restaurant to your doorstep
+                    </span>
+                    <span>
+                        Download Mohammed No Salad for better experience
+                    </span>
+                    <img src={appDownloadImage} />
+                </div>
             </div>
         </div>
     )
